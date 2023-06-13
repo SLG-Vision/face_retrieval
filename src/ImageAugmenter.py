e46b5d2e401bc00c89a8de6cc11d6ff0bb25f1ce
mich2k <miky3848@gmail.com>
2023-06-13 17:21:47 +0200

    adjusted kernel size for gaussian blur

--- src/ImageAugmenter.py
+++ src/ImageAugmenter.py
@@ -1,6 +1,6 @@
-import numpy as np
-import random
 import cv2
+import random
+import numpy as np
 
 class ImageAugmenter:
     def __init__(self):
@@ -27,71 +27,88 @@ class ImageAugmenter:
                 print(f"Transform '{transform_name}' not available.")
         return image
 
-    def resize(self, image, width, height):
+    def resize(self, image):
+        rows, cols = image.shape[:2]
+        width = random.randint(int(0.8 * cols), int(1.2 * cols))
+        height = random.randint(int(0.8 * rows), int(1.2 * rows))
         resized_image = cv2.resize(image, (width, height))
         return resized_image
 
-    def blur(self, image, kernel_size):
+    def blur(self, image):
+        kernel_size = random.randint(3, 9)
+        if kernel_size % 2 == 0:    # il kernel deve essere di dim. dispari
+            kernel_size += 1
         blurred_image = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
         return blurred_image
 
-    def motion_blur(self, image, kernel_size):
+    def motion_blur(self, image):
+        kernel_size = random.randint(3, 9)
         kernel = self.generate_motion_blur_kernel(kernel_size)
         motion_blur = cv2.filter2D(image, -1, kernel)
         return motion_blur
 
-    def rotate(self, image, angle):
+    def rotate(self, image):
+        angle = random.randint(-30, 30)
         rows, cols = image.shape[:2]
         M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
         rotated_image = cv2.warpAffine(image, M, (cols, rows))
         return rotated_image
 
     def flip(self, image):
-        flipped_image = cv2.flip(image, 1)
+        flip_code = random.randint(-1, 1)
+        flipped_image = cv2.flip(image, flip_code)
         return flipped_image
 
-    def adjust_brightness(self, image, factor):
+    def adjust_brightness(self, image):
+        factor = random.uniform(0.7, 1.3)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
-        v = cv2.add(v, factor)
+        v = cv2.multiply(v, factor)
         v = np.clip(v, 0, 255)
         adjusted_image = cv2.merge((h, s, v))
         adjusted_image = cv2.cvtColor(adjusted_image, cv2.COLOR_HSV2BGR)
         return adjusted_image
 
-    def adjust_contrast(self, image, factor):
+    def adjust_contrast(self, image):
+        factor = random.uniform(0.7, 1.3)
         adjusted_image = cv2.convertScaleAbs(image, alpha=factor, beta=0)
         return adjusted_image
 
-    def adjust_saturation(self, image, factor):
+    def adjust_saturation(self, image):
+        factor = random.uniform(0.7, 1.3)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
-        s = cv2.add(s, factor)
-        s = np.clip(s, 0, 255)
+        s = np.clip(s * factor, 0, 255).astype(np.uint8)
         adjusted_image = cv2.merge((h, s, v))
         adjusted_image = cv2.cvtColor(adjusted_image, cv2.COLOR_HSV2BGR)
         return adjusted_image
 
-    def zoom(self, image, zoom_factor):
+
+    def zoom(self, image):
+        scale = random.uniform(0.8, 1.2)
         rows, cols = image.shape[:2]
         cx = int(cols / 2)
         cy = int(rows / 2)
-        new_width = int(cols * zoom_factor)
-        new_height = int(rows * zoom_factor)
+        new_width = int(cols * scale)
+        new_height = int(rows * scale)
         x = cx - new_width // 2
         y = cy - new_height // 2
-        zoomed_image = cv2.resize(image[y:y+new_height, x:x+new_width], (cols, rows))
+        zoomed_image = image[y:y+new_height, x:x+new_width]
+        zoomed_image = cv2.resize(zoomed_image, (cols, rows))
         return zoomed_image
 
-    def tilt(self, image, angle):
+    def tilt(self, image):
+        angle = random.randint(-10, 10)
         rows, cols = image.shape[:2]
         M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
         tilted_image = cv2.warpAffine(image, M, (cols, rows))
         return tilted_image
 
-    def translate(self, image, shift_x, shift_y):
+    def translate(self, image):
+        shift_x = random.randint(-50, 50)
+        shift_y = random.randint(-50, 50)
         rows, cols = image.shape[:2]
-        M = np.float32([[1, 0, shift_x], [0, 1, shift_y]]) # type: ignore
+        M = np.float32([[1, 0, shift_x], [0, 1, shift_y]])  # type: ignore
         translated_image = cv2.warpAffine(image, M, (cols, rows))
         return translated_image
 
@@ -105,13 +122,16 @@ class ImageAugmenter:
 
 
 
-# test code
 
-image = cv2.imread("path_to_image.jpg") 
 
 augmenter = ImageAugmenter()
-transforms_list = ["resize", "blur", "zoom", "tilt"] 
-augmented_image = augmenter.apply_transforms(image, transforms_list)
-cv2.imshow("Augmented Image", augmented_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+transforms_list = ["resize", "blur", "zoom", "tilt"]
+vid = cv2.VideoCapture(0)
+
+while(True):
+    _, frame = vid.read()
+    cv2.imshow('Original Image', frame)
+    augmented_image = augmenter.apply_transforms(frame, transforms_list)
+    cv2.imshow("Augmented Image", augmented_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
